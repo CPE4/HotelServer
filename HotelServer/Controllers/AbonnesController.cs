187e7051600db8ce69472f5fda6b0776ed7e1bff
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2014-10-22 16:00:44 +0200

    Add controllers

---
+++ HotelServer/Controllers/AbonnesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HotelServer.Models;
+
+namespace HotelServer.Controllers
+{
+    public class AbonnesController : ApiController
+    {
+        private HotelEntities db = new HotelEntities();
+
+        // GET: api/Abonnes
+        public IQueryable<vAbonne> GetvAbonne()
+        {
+            return db.vAbonne;
+        }
+
+        // GET: api/Abonnes/5
+        [ResponseType(typeof(vAbonne))]
+        public IHttpActionResult GetvAbonne(decimal id)
+        {
+            vAbonne vAbonne = db.vAbonne.Find(id);
+            if (vAbonne == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vAbonne);
+        }
+
+        // PUT: api/Abonnes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutvAbonne(decimal id, vAbonne vAbonne)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vAbonne.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vAbonne).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!vAbonneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Abonnes
+        [ResponseType(typeof(vAbonne))]
+        public IHttpActionResult PostvAbonne(vAbonne vAbonne)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.vAbonne.Add(vAbonne);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = vAbonne.id }, vAbonne);
+        }
+
+        // DELETE: api/Abonnes/5
+        [ResponseType(typeof(vAbonne))]
+        public IHttpActionResult DeletevAbonne(decimal id)
+        {
+            vAbonne vAbonne = db.vAbonne.Find(id);
+            if (vAbonne == null)
+            {
+                return NotFound();
+            }
+
+            db.vAbonne.Remove(vAbonne);
+            db.SaveChanges();
+
+            return Ok(vAbonne);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool vAbonneExists(decimal id)
+        {
+            return db.vAbonne.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ HotelServer/Controllers/HotelsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HotelServer.Models;
+
+namespace HotelServer.Controllers
+{
+    public class HotelsController : ApiController
+    {
+        private HotelEntities db = new HotelEntities();
+
+        // GET: api/Hotels
+        public IQueryable<vHotel> GetvHotel()
+        {
+            return db.vHotel;
+        }
+
+        // GET: api/Hotels/5
+        [ResponseType(typeof(vHotel))]
+        public IHttpActionResult GetvHotel(decimal id)
+        {
+            vHotel vHotel = db.vHotel.Find(id);
+            if (vHotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vHotel);
+        }
+
+        // PUT: api/Hotels/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutvHotel(decimal id, vHotel vHotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vHotel.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vHotel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!vHotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Hotels
+        [ResponseType(typeof(vHotel))]
+        public IHttpActionResult PostvHotel(vHotel vHotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.vHotel.Add(vHotel);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = vHotel.id }, vHotel);
+        }
+
+        // DELETE: api/Hotels/5
+        [ResponseType(typeof(vHotel))]
+        public IHttpActionResult DeletevHotel(decimal id)
+        {
+            vHotel vHotel = db.vHotel.Find(id);
+            if (vHotel == null)
+            {
+                return NotFound();
+            }
+
+            db.vHotel.Remove(vHotel);
+            db.SaveChanges();
+
+            return Ok(vHotel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool vHotelExists(decimal id)
+        {
+            return db.vHotel.Count(e => e.id == id) > 0;
+        }
+    }
+}
